d9f4987f9b8f4319595d673b2058b03332e28fc4
Tiago Pimentel <tpimentelms@gmail.com>
2024-07-03 13:48:36 +0200

    Fix train transformer params

--- src/h02_learn/train_transformer.sh
+++ src/h02_learn/train_transformer.sh
@@ -14,9 +14,9 @@ fairseq-train --task ${TASK} \
 	--optimizer adam --adam-betas '(0.9, 0.98)' --weight-decay 0.01 --clip-norm 0.0 \
 	--lr 0.0005 --lr-scheduler inverse_sqrt --warmup-updates 4000 --warmup-init-lr 1e-07 \
 	--tokens-per-sample 512 --sample-break-mode none \
-	--max-tokens 8192 --update-freq 4 \
-	--cpu \
-	--max-update 100 --max-epoch 100 --patience 3  \
+	--max-tokens 32768 --update-freq 1 \
+	--fp16 \
+	--max-update 100000 --max-epoch 100 --patience 3  \
 	--seed ${RANDOM_SEED} \
 	--keep-last-epochs 1 \
 	--user-dir src/h02_learn/ --wandb-project fairseq-training \
